8a4d8f42e2bb9ebf45654a9e0b3480818fe060f8
anaysoul <soulyana_lakew@outlook.com>
2022-06-11 15:55:06 -0400

    communicating down table data

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'Soul', age: 33, job: 'Engineer' },
+    { name: 'Jill', age: 32, job: 'Designer' },
+    { name: 'Elyse', age: 25, job: 'Engineer' },
+  ];
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
