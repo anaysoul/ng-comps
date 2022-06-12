a87ca7d0afa4e4011e0cdbff99182e52593ef0aa
anaysoul <soulyana_lakew@outlook.com>
2022-06-12 06:49:18 -0400

    views module exercise

--- src/app/views/views-routing.module.ts
+++ src/app/views/views-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { ViewsHomeComponent } from './views-home/views-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ViewsHomeComponent,
+    // children: [
+    //   {
+    //     path: '',
+    //     component: StatisticsComponent,
+    //   },
+    // ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ViewsRoutingModule { }
+export class ViewsRoutingModule {}
